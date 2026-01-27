b04c9131c64f06c410ae93a79cf32e564083d7be
Fred <huertaserick@yahoo.ie>
2026-01-27 17:00:05 +0000

    adding data into SQL practice INSERT , SELECT and WHERE?

--- books_create_tables.sql
+++ books_create_tables.sql
@@ -4,8 +4,3 @@ CREATE TABLE books (
     author TEXT NOT NULL,
     year INT
 );
-
-INSERT INTO books (title, author, year) VALUES
-('The Hobbit', 'J.R.R. Tolkien', 1937),
-('Harry Potter and the Sorcerer''s Stone', 'J.K. Rowling', 1997),
-('Charlotte''s Web', 'E.B. White', 1952);
---
+++ books_seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO books (title, author, year) VALUES
+('The Hobbit', 'J.R.R. Tolkien', 1937),
+('Harry Potter and the Sorcerer''s Stone', 'J.K. Rowling', 1997),
+('Charlotte''s Web', 'E.B. White', 1952);
