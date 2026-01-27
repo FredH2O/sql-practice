23166db5c7de00ef637c53a349f013d5ed2f4f3b
Fred <huertaserick@yahoo.ie>
2026-01-27 16:13:33 +0000

    Added new db and sql for games. PRACTICE*

---
+++ games_create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS games (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    platform TEXT NOT NULL,
+    release_year INTEGER
+);
+
---
+++ games_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO games (title, platform, release_year) VALUES
+('The Witcher 3', 'PC', 2015),
+('Zelda: Breath of the Wild', 'Switch', 2017),
+('God of War', 'PS5', 2022),
+('Hollow Knight', 'PC', 2017),
+('Among Us', 'PC', 2018);
